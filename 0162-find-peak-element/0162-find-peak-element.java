f20f4d1cef558a69e16f26c6a403d196c3d974a3
Anjali Sanodiya <anjalisanodiya21@gmail.com>
2026-07-21 17:15:27 +0530

    Time: 0 ms (100%), Space: 44.3 MB (37.3%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,13 +1,21 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        int n= nums.length;
-        if(n==1) return 0;
-        if(nums[0]>nums[1])  return 0;
-        if(nums[n-1]>nums[n-2])  return n-1;
-
-        for(int i=1;i<n-1;i++){
-            if(nums[i]>nums[i-1] && nums[i]>nums[i+1])  return i;   
-        }
-        return -1;
+       int n=nums.length;
+       int si=1, ei=n-2;
+       if(n==1)  return 0;
+       if (nums[0] > nums[1]) return 0;
+       if (nums[n - 1] > nums[n - 2])  return n - 1;
+       while(si<=ei){
+         int mid= si+(ei-si)/2;
+         if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1])  return mid;
+   
+   // Pointer updation
+         if(nums[mid]<nums[mid-1]){
+         ei=mid-1;
+        } else{
+        si=mid+1;
+      }
+    }
+      return -1;
     }
 }
