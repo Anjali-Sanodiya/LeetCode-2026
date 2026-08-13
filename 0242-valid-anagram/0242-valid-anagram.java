d4ff7440767dd3a7e2cf414627068559aa4ce571
Anjali Sanodiya <anjalisanodiya21@gmail.com>
2026-08-13 14:42:36 +0530

    Time: 25 ms (5.23%), Space: 47.2 MB (5.12%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length()!=t.length())   return false;
+
+        int n=s.length();
+
+        HashMap<Character, Integer> map1= new HashMap<>();
+        HashMap<Character, Integer> map2= new HashMap<>();
+
+        for(int i=0;i<n;i++){
+            map1.put(s.charAt(i), map1.getOrDefault(s.charAt(i),0)+1);
+            map2.put(t.charAt(i), map2.getOrDefault(t.charAt(i),0)+1);
+        }
+        for(int i=0;i<n;i++){
+            if(!map1.getOrDefault(s.charAt(i),0).equals(map2.getOrDefault(s.charAt(i),0)))  return false;
+        }
+        return true;
+    }
+}
